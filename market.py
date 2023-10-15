8e846912bc437ccabae8780947d920be88246754
Ruchit Soni <ruchitsoni1898@gmail.com>
2023-10-15 22:01:21 +0200

    create base file and changes in home and market page

--- market.py
+++ market.py
@@ -9,11 +9,12 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items =[{"id":1,"Name":"phone","Barcode":"893212299897","price":500},
-    {"id":2,"Name":"Laptop","Barcode":"123985473165","price":900},
-    {"id":3,"Name":"Keyboard","Barcode":"231985128446","price":150}]
-
-    return render_template('market.html',items=items)
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
 
 if __name__ == '__main__':
-    app.run(debug=1)
+    app.run(debug=True)
